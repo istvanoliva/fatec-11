421838b56bd8bcb7d723b0addcacd83574edd567
Istvan <olivaistvan@gmail.com>
2024-11-17 23:57:25 -0300

    feat: implements sp_cadastrar_cliente

--- exercicio11.sql
+++ exercicio11.sql
@@ -3,3 +3,18 @@ CREATE TABLE tb_log (
     data_hora TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     nome_procedimento VARCHAR(200)
 );
+
+CREATE OR REPLACE PROCEDURE sp_cadastrar_cliente (IN nome VARCHAR(200), IN codigo INT DEFAULT NULL)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    IF codigo IS NULL THEN
+        INSERT INTO tb_cliente (nome) VALUES (nome);
+    ELSE
+        INSERT INTO tb_cliente (codigo, nome) VALUES (codigo, nome);
+    END IF;
+
+    -- Inserir registro no log
+    INSERT INTO tb_log (nome_procedimento) VALUES ('sp_cadastrar_cliente');
+END;
+$$;
